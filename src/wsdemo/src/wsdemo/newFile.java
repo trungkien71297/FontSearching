5039a9f016c98dc0f1a9a723bf806438e56312ae
Peemoti <trungkien71297@gmail.com>
2017-11-04 09:02:24 +0700

    Edited by Kien
    File newFile.java edited

---
+++ src/wsdemo/src/wsdemo/newFile.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsdemo;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
+
+/**
+ *
+ * @author ngocnt
+ */
+public class WSDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GeoIPService pogba =  new GeoIPService();
+        GeoIPServiceSoap rooney = pogba.getGeoIPServiceSoap();
+        GeoIP ferguson = rooney.getGeoIP("123.30.215.62");
+        System.out.println(ferguson.getCountryName());
+        System.out.println(ferguson.getReturnCodeDetails());
+		System.out.println("Edited");
+    }
+    
+}
